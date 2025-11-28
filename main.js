a159e35c393a6b0b7321af150053f03165135af9
Courtney <courtneyjanegray1624@gmail.com>
2025-11-28 22:05:25 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+const container = document.querySelector('.container')
+const serach = document.querySelector('.search-box button')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetails = document.querySelector('.weather-details')
+const error404 = document.querySelector('.not-found')
+
+serach.addEventListener('click',() =>{
+  //the api key and the input from the user
+  const APIKey ='dd29559092b4b1fde04863d90ed5a92b';
+  const city = document.querySelector('.search-box input').value;
+
+  if(city ==='')
+      return;
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json =>{
+     //if the input enetered isnt valid it shows the 404 error, .not-found
+    if(json.cod === '404'){
+        container.style.height = '400px';
+        weatherBox.style.display = 'none';
+        weatherDetails.style.display = 'none';
+        error404.style.display = 'block';
+        error404.classList.add('fadeIn');
+        return;
+      }
+      //removes the class when its not a 404 error
+      error404.style.display = 'none';
+      error404.classList.remove('fadeIn');
+
+      const image = document.querySelector('.weather-box img');
+      const temperature = document.querySelector('.weather-box .temperature');
+      const description = document.querySelector('.weather-box .description');
+      const humidity = document.querySelector('.weather-details .humidity span');
+      const wind = document.querySelector('.weather-details .wind span');
+      //changes the image to the appropriate image corrisponding with the current weather
+      switch(json.weather[0].main){
+        case 'Clear':
+          image.src ='images/clear.png';
+          break;
+        case 'Rain':
+          image.src ='images/rain.png';
+          break;
+        case 'Snow':
+          image.src ='images/snow.png';
+          break;
+        case 'Clouds':
+          image.src ='images/cloud.png';
+          break;
+        case 'Haze':
+          image.src ='images/mist.png';
+          break;
+        default:
+            image.src ='';
+
+      }
+      //adds the weather details from the inputted location
+
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+      weatherBox.style.display ='';
+      weatherDetails.style.display = '';
+      weatherBox.classList.add('fadeIn');
+      weatherDetails.classList.add('fadeIn');
+      container.style.height = '590px';
+
+
+
+  });
+  
+
+});
